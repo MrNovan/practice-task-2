09e4cd9b97ed25fdd9cea2e88abcbe4b11e64a71
MrNovan <toni.justdoit@bk.ru>
2024-06-05 15:54:17 +0300

    add Makefile

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const btn = document.querySelector('.btn');
 const close = document.querySelector('.close');
 
 // Открываем модальное окно при нажатии на кнопку
-btn.onclick = function () {
+
+btn.onclick = function () {// eslint-disable-line
   modal.style.display = 'block';
 };
 
 // Закрываем модальное окно при нажатии на крестик
-close.onclick = function () {
+close.onclick = function () {// eslint-disable-line
   modal.style.display = 'none';
 };
 
 // Закрываем модальное окно при клике за его пределами
-window.onclick = function (event) {
+window.onclick = function (event) {// eslint-disable-line
   if (event.target === modal) {
     modal.style.display = 'none';
   }
