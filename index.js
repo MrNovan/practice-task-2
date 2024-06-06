85ce97ce1da4690a30ae1f9a8fbc445c70de8ac5
MrNovan <toni.justdoit@bk.ru>
2024-06-06 13:24:24 +0300

    fix file

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const modal = document.getElementById('modal');
 const btn = document.querySelector('.btn');
 const close = document.querySelector('.close');
 
-// Открываем модальное окно при нажатии на кнопку
-
 btn.onclick = function () {// eslint-disable-line
   modal.style.display = 'block';
 };
 
-// Закрываем модальное окно при нажатии на крестик
 close.onclick = function () {// eslint-disable-line
   modal.style.display = 'none';
 };
 
-// Закрываем модальное окно при клике за его пределами
 window.onclick = function (event) {// eslint-disable-line
   if (event.target === modal) {
     modal.style.display = 'none';
